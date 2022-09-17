2e39446bab93fbe6b3c133b1c319551569f9dfd5
tony-lee <tony@riafox.com>
2022-09-17 20:14:48 +0900

    fix some issues

--- src/components/ProductCard/index.js
+++ src/components/ProductCard/index.js
@@ -11,8 +11,8 @@ const ProductCard = (props) => {
         alt={product.caption}
       />
       <h6 className="mt-2">{product.imgText}</h6>
-      {size != "sm" && <h4>{product.caption}</h4>}
-      {size == "sm" && <h5>{product.caption}</h5>}
+      {size !== "sm" && <h4>{product.caption}</h4>}
+      {size === "sm" && <h5>{product.caption}</h5>}
       <h6>{product.description}</h6>
     </div>
   );
